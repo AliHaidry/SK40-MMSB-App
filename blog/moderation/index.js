9a352a01dbe076a6135f9768c2f1c9bbd63e8b94
AliHaidry <alihaidry11@gmail.com>
2021-03-29 14:31:01 -0400

    code update with 'moderation--service : index.js'

--- blog/moderation/index.js
+++ blog/moderation/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bodyParser = require('bodyParser');
+const bodyParser = require('body-parser');
 const axios = require('axios');
 
 // new express application
@@ -11,5 +11,10 @@ app.use(bodyParser.json());
 /** this service watch for events */
 
 app.post('/events', (req, res)=>{
-    
+
+});
+
+
+app.listen(4003, ()=>{
+    console.log('Listening on Port 4003');
 });
